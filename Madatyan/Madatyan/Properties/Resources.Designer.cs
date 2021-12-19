5f41805b14cfe324888fd332ad7ecb4ee74cf7d1
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-19 18:14:30 +0300

    Update Resources.Designer.cs

--- Madatyan/Madatyan/Properties/Resources.Designer.cs
+++ Madatyan/Madatyan/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace Turvandyan.Properties
+namespace Madatyan.Properties
 {
     /// <summary>
     ///   Класс ресурсов со строгим типом для поиска локализованных строк и пр.
@@ -43,7 +43,7 @@ namespace Turvandyan.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Turvandyan.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Madatyan.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
